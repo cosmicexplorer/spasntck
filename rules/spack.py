ba01d2638daefbc95ce77468b63eff24d145bd8a
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2020-11-18 23:33:14 -0800

    admit the git checkout rules were unnecessary and get over it

--- rules/spack.py
+++ rules/spack.py
@@ -1,13 +1,24 @@
-from pants.engine.rules import Get, collect_rules, rule
-# from pants.backend.python.subsystems.python_tool_base import PythonToolBase
+from pants.core.util_rules.external_tool import (
+    DownloadedExternalTool,
+    ExternalToolRequest,
+    TemplatedExternalTool,
+)
+from pants.engine.rules import Get, SubsystemRule, collect_rules, rule
 
 
-# class Spack(PythonToolBase):
-#   """The build tool for HPC (https://spack.io)."""
+class Spack(TemplatedExternalTool):,
+  """The build tool for HPC (https://spack.io)."""
 
-#   options_scope = "spack-config"
-#   default_version = "spack==0.16.0"
+  options_scope = "spack-config"
+  default_version = "0.16.0"
+  default_known_versions = [
+    "0.16.0|darwin|064b2532c70916c7684d4c7c973416ac32dd2ea15f5c392654c75258bfc8c6c2|5512800",
+    "0.16.0|linux|064b2532c70916c7684d4c7c973416ac32dd2ea15f5c392654c75258bfc8c6c2|5512800",
+  ]
+  default_url_template = (
+    "https://github.com/spack/spack/releases/download/v{version}/spack-{version}.tar.gz"
+  )
 
 
 def rules():
-  return [*collect_rules()]
+  return [SubsystemRule(Spack), *collect_rules()]
